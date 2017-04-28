6a482c894b9e004591e20d5544aba6698b874fbc
seaneil01 <seaneil01@users.noreply.github.com>
2017-04-27 23:43:25 -0500

    upto directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,14 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
     @director = Director.find(params[:id])
   end
 
-  def new
+  def new_form
+    render("directors/new_form.html.erb")
   end
 
   def create_row
@@ -19,7 +20,7 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    redirect_to("/directors/#{@director.id}")
   end
 
   def edit_form
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,23 +4,30 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find(params[:id])
   end
 
   def new_form
+    render("movies/new_form.html.erb")
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    new_movie = Movie.new
+    movie_title = params[:title]
+    movie_year = params[:year]
+    movie_duration = params[:duration]
+    movie_description = params[:description]
+    movie_image_url = params[:image_url]
+    movie_director_id = params[:director_id]
+    new_movie.title = movie_title
+    new_movie.year = movie_year
+    new_movie.duration = movie_duration
+    new_movie.description = movie_description
+    new_movie.image_url = movie_image_url
+    new_movie.director_id = movie_director_id
+    new_movie.save
 
-    @movie.save
-
-    render("show")
+    redirect_to("/movies/#{new_movie.id}")
   end
 
   def edit_form
@@ -28,19 +35,28 @@ class MoviesController < ApplicationController
   end
 
   def update_row
-    @movie.title = params[:title]
-    @movie.year = params[:year]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    edit_movie =  Movie.find(params[:id])
+    movie_title = params[:title]
+    movie_year = params[:year]
+    movie_duration = params[:duration]
+    movie_description = params[:description]
+    movie_image_url = params[:image_url]
+    movie_director_id = params[:director_id]
 
-    render("show")
+    edit_movie.title = movie_title
+    edit_movie.year = movie_year
+    edit_movie.duration = movie_duration
+    edit_movie.description = movie_description
+    edit_movie.image_url = movie_image_url
+    edit_movie.director_id = movie_director_id
+    edit_movie.save
+
+    redirect_to("/movies/#{edit_movie.id}")
   end
 
   def destroy
     movie = Movie.find(params[:id])
-
+    @movie_title = movie.title
     movie.destroy
   end
 end
