0f42b80df01513adc531dc6ef6fa826fece83d2b
seaneil01 <seaneil01@users.noreply.github.com>
2017-05-02 14:18:41 -0500

    directors fixed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -28,16 +28,15 @@ class DirectorsController < ApplicationController
   end
 
   def update_row
-    @director = Director.find(params[:id])
-
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director =  Director.find(params[:id])
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
-    render("show")
+    redirect_to("/directors/#{@director.id}")
   end
 
   def destroy
